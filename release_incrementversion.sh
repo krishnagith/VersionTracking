a42d18e8c57708b81a52039954e6dd6dafc34cd0
krishnagith <34870826+krishnagith@users.noreply.github.com>
2021-05-04 22:01:53 +1000

    Create release_incrementversion.sh
    adding count to the version tracking

---
+++ release_incrementversion.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+WORKSPACE=$1
+
+baseversion=`cat ${WORKSPACE}/Baseversion/Baseversion`
+versiontracking=`cat ${WORKSPACE}/VersionTracking/Versiontracking`
+
+baserootval=`cat ${WORKSPACE}/Baseversion/Baseversion | cut -d"." -f1`
+baserootval2=`cat ${WORKSPACE}/Baseversion/Baseversion | cut -d"." -f2`
+
+versiontrackingval=`cat ${WORKSPACE}/VersionTracking/Versiontracking|head -n 1`
+
+echo $baserootval $baserootval2 $versionrootval $versionrootval2 $count
+
+   echo " base verison is not matched version tracking number"
+   count=1
+   echo "newversion=$versiontrackingval-$count"  > ${WORKSPACE}/versionupdated.properties
+
+    
+cd ${WORKSPACE}/java-jsp-diary/
+cp pom.xml backuppom.xml
+
+newversion=`cat ${WORKSPACE}/versionupdated.properties |  cut -d"=" -f2`
+
+versionProject=`/usr/bin/mvn help:evaluate -Dexpression=project.version | tail -7 | head -1`
+
+echo $versionProject
+
+sed -i "s#<version>$versionProject</version>#<version>$newversion</version>#g" pom.xml
+finalversion=`/usr/bin/mvn help:evaluate -Dexpression=project.version | tail -7 | head -1`
+
+echo "Final version to build: $finalversion"
+
+
+
